0db202dd1765fc867bddb313fc480454e894c77b
chandragirisneha0412 <chandragirisneha0412@gmail.com>
2023-02-10 12:51:31 +0530

    created 3rd component

--- Employee List/sneha-angular-demo/src/app/app-routing.module.ts
+++ Employee List/sneha-angular-demo/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [
-  {path:'Employee' ,component:EmployeeComponent}
+  {path:'employee' ,component:EmployeeComponent},
+  {path:"home",component:HomeComponent},
+  {path:"login",component:LoginComponent}
 ];
 
 @NgModule({
